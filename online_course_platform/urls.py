1419eb5abffa1143f85f715f9ea4d7ec95db66e8
Serhii <serhii.domik@gmail.com>
2024-07-02 18:37:39 +0300

    fixed namespace

--- online_course_platform/urls.py
+++ online_course_platform/urls.py
@@ -20,5 +20,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", include("course.urls", namespace="blog")),
+    path("", include("course.urls", namespace="course")),
 ]
