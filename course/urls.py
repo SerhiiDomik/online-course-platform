b5d829d44bdcd8c4c733633a34910cd0815456c7
Serhii <serhii.domik@gmail.com>
2024-07-02 20:38:47 +0300

    added urls course-detail & lesson-detail

--- course/urls.py
+++ course/urls.py
@@ -1,9 +1,19 @@
 from django.urls import path
-from .views import IndexView
+from .views import IndexView, CourseDetailView, LessonDetailView
 
 
 app_name = "course"
 
 urlpatterns = [
     path("", IndexView.as_view(), name="index"),
+    path(
+        "course/<int:course_pk>/",
+        CourseDetailView.as_view(),
+        name="course-detail"
+    ),
+    path(
+        "course/<int:course_pk>/lesson/<int:lesson_pk>/",
+        LessonDetailView.as_view(),
+        name="lesson-detail"
+    )
 ]
