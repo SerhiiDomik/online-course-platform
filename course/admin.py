c29a8223d4d57bb784c522d8f3b49d120957f6c0
Serhii <serhii.domik@gmail.com>
2024-07-02 18:54:13 +0300

    created admin.py with UserAdmin CourseAdmin LessonAdmin

--- course/admin.py
+++ course/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from .models import User, Course, Lesson
+
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    search_fields = ("username", "email",)
+    list_filter = ("username",)
+    list_display = UserAdmin.list_display + ("username",)
+
+
+@admin.register(Course)
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ("name",)
+    list_filter = ("name",)
+
+
+@admin.register(Lesson)
+class LessonAdmin(admin.ModelAdmin):
+    list_display = ("title", "course")
+    list_filter = ("course",)
