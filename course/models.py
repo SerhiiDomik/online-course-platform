a83aac4bfd7695db96dc22d59f9f8cdd27b16008
Serhii <serhii.domik@gmail.com>
2024-07-02 18:26:17 +0300

    created models: User Course Lesson

--- course/models.py
+++ course/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class User(AbstractUser):
+    username = models.CharField(max_length=30, unique=True)
+
+
+class Course(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+
+class Lesson(models.Model):
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    course = models.ForeignKey(Course, on_delete=models.CASCADE, related_name='lessons')
+
+    def __str__(self):
+        return self.title
